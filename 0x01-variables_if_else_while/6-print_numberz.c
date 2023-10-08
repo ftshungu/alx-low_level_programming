049ab1883ee7dc503072f664e83b2d454ac59b0c
Fundile <ftshungu@gmail.com>
2023-10-08 15:44:55 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This is the main fucntion of the program.
+ * Its prints all singel digit numbers of base 10 starting from 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
