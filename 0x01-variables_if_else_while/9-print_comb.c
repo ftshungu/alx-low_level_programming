3ccbb676458a34d14eb83ef73a1f4f130cb9d923
Fundile <ftshungu@gmail.com>
2023-10-08 16:42:27 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This is the main fucntion of the program.
+ * Its prints all the numbers of base 16 in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 9 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
