4b644d812dfd085606c3c8e8e5dd13368987646c
Fundile <ftshungu@gmail.com>
2023-10-10 10:50:24 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: the program will print putchar, followed by a new line.
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	int (putchar);
+	{
+		printf("putchar\n");
+	}
+	return (0);
+}
