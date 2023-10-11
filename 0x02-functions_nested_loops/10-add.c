f2ea28263bc9464da65d73f192cf23e58628c9ce
Fundile <ftshungu@gmail.com>
2023-10-11 08:16:44 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -  add two integers and and return the results
+ *@a: parameter 1
+ *@b: parameter 2
+ *Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
