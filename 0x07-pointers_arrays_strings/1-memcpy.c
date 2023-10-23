cf6b0cf313b427c42110cb99f5a451f5aa0f9daa
Fundile <ftshungu@gmail.com>
2023-10-23 05:46:11 -0700

    My first commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes
+ * return: pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
